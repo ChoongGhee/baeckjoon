953617921bd1a40a4ec0e5565d43b6e4697cb6b5
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2024-07-09 21:38:26 +0900

    [Silver II] Title: 외판원 순회 2, Time: 5136 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -0,0 +1,28 @@
+import sys
+
+def TSP(start, now, sum, cnt):
+    global ans
+    if cnt == n:
+        if W[now][start]:
+            if sum + W[now][start] < ans:
+                ans = sum + W[now][start]
+    for i in range(n):
+        if not flag[i] and W[now][i]:
+            flag[i] = True
+            TSP(start, i, sum + W[now][i], cnt + 1)
+            flag[i] = False
+
+n = int(input())
+W = []
+for i in range(n):
+    W.append(list(map(int, input().split())))
+
+flag = [False] * n
+ans = sys.maxsize
+
+for i in range(n):
+    flag[i] = True
+    TSP(i, i, 0, 1)
+    flag[i] = False
+
+print(ans)
