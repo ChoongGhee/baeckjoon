7b46cb8e37927dc52bb9452327389769dba5ceb0
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2024-07-10 10:33:43 +0900

    Update 나무 자르기.py

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,20 +1,28 @@
 n, min_take = map(int, input().split())
 tree = list(map(int, input().split()))
+# 내림차순 정렬
 tree.sort(reverse=True)
 
+# 내림차로 정렬된 나무들의 차이 리스트
 offset = [tree[i] - tree[i + 1] for i in range(len(tree) - 1)]
-offset.append(tree[-1])  # 마지막 나무의 전체 높이를 추가
+# 마지막 나무 높이 추가 (마지막 나무 - 지면(0m))
+offset.append(tree[-1])
 
 
 def find_max_height():
     sum = 0
     for startidx in range(len(offset)):
         if offset[startidx] * (startidx + 1) + sum >= min_take:
+            # 조건이 만족하면 남은 부분을 계산
             remaining = min_take - sum
+            # offset값 기준으로 자를 위치 계산(올림으로 최소의 M을 만족시킴)
             cut = (remaining + startidx) // (startidx + 1)
+            # 자른 위치 반환
             return tree[startidx] - cut
+        # 자른 총량 계산
         sum += offset[startidx] * (startidx + 1)
-    return 0  # 모든 나무를 다 잘라도 목표량에 도달하지 못한 경우
+    # 나무를 다 자랐음에도 min_take가 만족하지 않았으면 0반환
+    return 0
 
 
 result = find_max_height()
