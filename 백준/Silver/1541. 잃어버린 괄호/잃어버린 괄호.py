b8894241ba03b0dc26e8c4b81ceae65b8fc4f20f
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2024-07-22 20:51:31 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,17 +1,26 @@
 import sys
 
 if __name__ == "__main__":
-    string = sys.stdin.readline().strip()
-    
+    input = sys.stdin.readline
+
+    string = input().strip()
+
     minus_split = string.split("-")
-    
+
     if len(minus_split) == 1:
-        sum1 = sum(int(x) for x in minus_split[0].replace("+", " ").split())
+        sum1 = 0
+        p_split = minus_split[0].split("+")
+        for i in p_split:
+            sum1 += int(i)
         print(sum1)
     else:
-        result = sum(int(x) for x in minus_split[0].replace("+", " ").split())
-        
+        sum = sum(map(int, minus_split[0].split("+")))
+
         for i in minus_split[1:]:
-            result -= sum(int(x) for x in i.replace("+", " ").split())
-        
-        print(result)
+            p_split = i.split("+")
+            p_sum = 0
+            for j in p_split:
+                p_sum += int(j)
+            sum -= p_sum
+
+        print(sum)
