337d34a0de8b64a537f0b39d517f375ae94f6a6e
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2024-08-06 23:05:16 +0900

    [level 3] Title: 정수 삼각형, Time: 111.25 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 14.7 MB, 시간: 112.39 ms
+메모리: 14.6 MB, 시간: 111.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 11:10:58
+2024년 08월 06일 23:05:10
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,28 +1,26 @@
 def solution(triangle):
     n = len(triangle)
 
-    dp = [[0]] + triangle
-
-    for i in range(1, n + 1):
-        for j in range(i):
-            if i == 1:
+    for i in range(n):
+        for j in range(i+1):
+            if i == 0:
                 continue
-            elif i == 2:
-                dp[i][j] += dp[i - 1][0]
+            elif i == 1:
+                triangle[i][j] += triangle[i - 1][0]
             else:
                 a = []
                 for k in range(2):
                     if j == 0:
-                        a.append(dp[i - 1][0])
-                        # a.append(dp[i - 1][1])
-                    elif j == i - 1:
-                        # a.append(dp[i - 1][j - 2])
-                        a.append(dp[i - 1][j - 1])
+                        a.append(triangle[i - 1][0])
+                    # a.append(triangle[i - 1][1])
+                    elif j == i:
+                    # a.append(triangle[i - 1][j - 2])
+                        a.append(triangle[i - 1][j - 1])
                     else:
-                        a.append(dp[i - 1][j - 1])
-                        a.append(dp[i - 1][j])
+                        a.append(triangle[i - 1][j - 1])
+                        a.append(triangle[i - 1][j])
 
-                dp[i][j] += max(a[0], a[1])
+                triangle[i][j] += max(a[0], a[1])
 
-    answer = max(dp[n])
+    answer = max(triangle[-1])
     return answer
