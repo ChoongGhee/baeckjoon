978614a6bead1cf46f3c93ee20f94507de321a85
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2024-09-13 17:30:44 +0900

    [Bronze V] Title: A+B - 3, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+
+    for i in range(n):
+        a, b = map(int, input().split())
+        print(a + b)
