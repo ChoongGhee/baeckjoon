7f2cca5f53d89e103ad13baf96fcfe06070ab710
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2025-02-04 22:27:21 +0900

    [Unrated] Title: 0.1, Time: 64 ms, Memory: 32412 KB, Score: 1254 point -BaekjoonHub

---
+++ 백준/Unrated/11921. 0．1/0．1.py
@@ -0,0 +1,17 @@
+import sys
+import time
+
+total = 0
+cnt = 0
+N = int(sys.stdin.readline())
+end_time = time.perf_counter() + 0.1
+
+try:
+   while cnt < N and time.perf_counter() < end_time:
+       total += int(sys.stdin.readline())
+       cnt += 1
+except:
+   pass
+
+print(cnt)
+print(total)
